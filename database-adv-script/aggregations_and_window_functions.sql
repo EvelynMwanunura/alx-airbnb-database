43e65ef242454608ebd68cc2a770ec20e6a615af
EvelynMwanunura <evelynmwanunura@gmail.com>
2025-09-05 20:01:07 +0200

    Use a window function (ROW_NUMBER, RANK) to rank properties based on the total number of bookings they have received.

--- database-adv-script/aggregations_and_window_functions.sql
+++ database-adv-script/aggregations_and_window_functions.sql
@@ -15,14 +15,16 @@ WITH property_bookings AS (
         p.name,
         COUNT(b.booking_id) AS total_bookings
     FROM Property p
-    LEFT JOIN Booking b ON p.property_id = b.property_id
+    LEFT JOIN Booking b 
+        ON p.property_id = b.property_id
     GROUP BY p.property_id, p.name
 )
-SELECT 
+SELECT
     property_id,
     name,
     total_bookings,
+    RANK() OVER (ORDER BY total_bookings DESC) AS rank,
     ROW_NUMBER() OVER (ORDER BY total_bookings DESC) AS row_number
 FROM property_bookings
-ORDER BY row_number;
+ORDER BY total_bookings DESC;
 
